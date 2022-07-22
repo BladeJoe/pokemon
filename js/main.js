2c00aec6d30cf97f479f8430a5c923d1474d5324
Joe <106337992+BladeJoe@users.noreply.github.com>
2022-07-22 16:11:51 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let elList = document.querySelector(".list");
+
+
+
+function renderPokemons(array) {
+    elList.innerHTML = null
+
+    for (let item of array) {
+        let newLi = document.createElement("li");
+        newLi.classList.add("card");
+        let newImg = document.createElement("img");
+        newImg.src = item.img;
+
+        let newH3 = document.createElement("h3");
+        newH3.textContent = item.name;
+
+        let newh5 = document.createElement("h5")
+        newh5.innerHTML = `Type: ${item.type} <br>
+        Height: ${item.height} <br> Weight: ${item.weight}`;
+
+
+        newLi.appendChild(newImg);
+        newLi.appendChild(newH3);
+        newLi.appendChild(newh5);
+
+        elList.appendChild(newLi)
+
+    }
+}
+
+renderPokemons(pokemons)
